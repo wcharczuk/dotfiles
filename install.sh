29fc497ffbae644d4128aa1f4766a2a1eb73d6e9
Will Charczuk <will@clotheshor.se>
2015-11-02 23:38:32 +0000

    install tweak.

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ target="$HOME/.$name"
 if [ -e "$target" ]; then
     rm "$target"
 fi
-if [ "$name" != 'install.sh' ] && [ "$name" != 'assets' ]; then
+if [ "$name" != 'install.sh' ] && [ "$name" != 'assets' ] && [ "$name" != 'README.md' ]; then
   echo "Creating $target"
     ln -s "$PWD/$name" "$target"
 fi
