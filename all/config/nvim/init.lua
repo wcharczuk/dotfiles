e58ca93d3f6aa1759325b043bc7fb32fc80435c2
Will Charczuk <wcharczuk@users.noreply.github.com>
2025-08-03 08:26:41 -0700

    neovim config tweaks (work?)

--- all/config/nvim/init.lua
+++ all/config/nvim/init.lua
@@ -1,24 +1,35 @@
-vim.cmd('colorscheme habamax')
-
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 
 -- Auto-install lazy.nvim if not present
-if not vim.uv.fs_stat(lazypath) then
-  print('Installing lazy.nvim....')
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system({ "git",
+  "clone",
+  "--filter=blob:none",
+  "--branch=stable", 
+  lazyrepo,
+  lazypath
   })
-  print('Done.')
 end
+vim.opt.rtp:prepend(lazypath)
 
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function () 
+          local configs = require("nvim-treesitter.configs")
+
+          configs.setup({
+              ensure_installed = { "c", "lua", "vim", "vimdoc", "go", "typescript", "javascript", "html", "python" },
+              sync_install = false,
+              highlight = { enable = true },
+              indent = { enable = true },  
+            })
+        end
+    },
 	{'folke/tokyonight.nvim'},
 	{
         "ibhagwan/fzf-lua",
